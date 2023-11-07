81b45468ffc71312eb922147eb06c4224b6e595f
treblot <treblotnad@gmail.com>
2023-11-07 16:42:52 -0600

    functionality all works, need to deploy, fix README and potentially add special conditions for password requirements

--- script.js
+++ script.js
@@ -23,32 +23,37 @@ function generatePassword(){
   var passwordUpp = confirm("Do you want capital letter in your password?");
   var passwordNum = confirm("Do you want numbers in your password?");
   var passwordLow = confirm("Do you want lower case letter in your password?");
+
+  if(!passwordSym && !passwordUpp && !passwordNum && !passwordLow){
+    return "Password must use at least 1 set up characters";
+  } 
+
   var upperCase = ['A', 'B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
   var lowerCase =['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
-  var symbols = ['!','"','#','$','%','&',"'",'(',')','*','+','-', '.', '/', ':', `;`, `<`, `=`, `>`, `?`, `@`, "[", `]`, `^`, `_`, `{`, `|`, `}`, `~`];
+  var symbols = ['!','"','#','$','%','&',"'",'(',')','*','+','-', '.', ',', '/', ':', `;`, `<`, `=`, `>`, `?`, `@`, "[", `]`, `^`, `_`, `{`, `|`, `}`, `~`];
   var numbers = ['0','1','2','3','4','5','6','7','8','9'];
-  var passBank = "";
+  var passBank = [];
   var pass ="";
 
-  if (passwordLow = true){
+  if (passwordLow == true){
     passBank = passBank.concat(lowerCase);
   }
-  if (passwordSym = true){
+  if (passwordSym == true){
     passBank = passBank.concat(symbols);
   }
-  if (passwordUpp = true){
+  if (passwordUpp == true){
     passBank = passBank.concat(upperCase);
   }
-  if (passwordNum = true){
+  if (passwordNum == true){
     passBank = passBank.concat(numbers)
   }
 
   for (let i=0;i<passwordLength;i++){
-    pass = pass + passBank[Math.floor(Math.random()*passBank.length)]
+    pass = pass + passBank[Math.floor(Math.random()*passBank.length)];
   }
 
 
 
-  return upperCase[Math.floor(Math.random()*upperCase.length)];
+  return pass;
 
 }
